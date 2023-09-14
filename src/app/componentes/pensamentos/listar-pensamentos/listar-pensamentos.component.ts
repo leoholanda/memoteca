04136d0fe4cd8dcd914e1d934f519ad3c8307fb9
Leonardo Holanda Arruda <leoholanda23@gmail.com>
2023-09-14 10:44:52 -0400

    Lista de pensamentos favoritos

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -13,36 +14,63 @@ export class ListarPensamentosComponent implements OnInit {
   paginaAtual: number = 1;
   temMaisPensamento: boolean = true;
   filtro: string = '';
+  favoritos: boolean = false;
+  listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural';
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.listarPensamentos()
   }
 
   listarPensamentos() {
-    this.service.listar(this.paginaAtual, this.filtro).subscribe((pensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((pensamentos) => {
       this.listaPensamentos = pensamentos;
     });
   }
 
+  listarFavoritos() {
+    this.temMaisPensamento = true;
+    this.paginaAtual = 1;
+    this.favoritos = true;
+    this.titulo = 'Meus Favoritos';
+
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((pensamentos) => {
+        this.listaPensamentos = pensamentos;
+        this.listaFavoritos = pensamentos;
+      });
+  }
+
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual, this.filtro)
+    this.service.listar(++this.paginaAtual, this.filtro, this.favoritos)
       .subscribe(pensamentos => {
         this.listaPensamentos.push(...pensamentos);
         if(!pensamentos.length) {
           this.temMaisPensamento = false;
         }
-      })
+      });
   }
 
   pesquisarPensamentos() {
     this.temMaisPensamento = true;
     this.paginaAtual = 1;
-    this.service.listar(this.paginaAtual, this.filtro)
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
       .subscribe(listaPensamentos => {
         this.listaPensamentos = listaPensamentos
-      })
+      });
+  }
+
+  recarregarComponente() {
+    this.favoritos = false;
+    this.paginaAtual = 1;
+
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
   }
 
 }
